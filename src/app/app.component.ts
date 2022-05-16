aa35b75afa480e4d8bf94b1d14f83f3e1f841bc9
Oliksii Nakhaiev <a.nakhaev@gmail.com>
2022-05-16 08:34:58 +0300

    first component, creation of components, templates, styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-course';
+  name = 'Alex';
+  title = 'Just for test'
 }
