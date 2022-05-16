8ba72de81715c90e9a506bd3f16007b281630674
Oliksii Nakhaiev <a.nakhaev@gmail.com>
2022-05-16 08:43:31 +0300

    styles as a separate file or styles inside component.ts

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.scss']
+  styles: [`p { color: darkmagenta}`]
 })
 export class ServerComponent {
 }
